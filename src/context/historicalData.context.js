70bf2ae19476a8b797912957edda88ec35851d17
gajanandh <80502737+gajanandh@users.noreply.github.com>
2021-11-22 12:38:23 +0530

    Update API URL

--- src/context/historicalData.context.js
+++ src/context/historicalData.context.js
@@ -8,7 +8,7 @@ export function TimeDataProvider(props) {
 const [data,setData]=useState();
   useEffect( ()=>{
     const gettimedata = async()=>{
-        const res =await  fetch('https://api.covid19india.org/v4/min/timeseries.min.json')
+        const res =await  fetch('https://data.covid19india.org/v4/min/timeseries.min.json')
         const data  = await  res.json()
         setData(data)   
     }
